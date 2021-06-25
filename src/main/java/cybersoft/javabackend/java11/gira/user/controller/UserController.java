342415d1359b6cb27ad60af5d8c7d5310cbffa73
lyhuynhvu <lyhuynhvu2906@gmail.com>
2021-06-25 21:40:47 +0700

    config user module
    create and config user functions

--- src/main/java/cybersoft/javabackend/java11/gira/user/controller/UserController.java
+++ src/main/java/cybersoft/javabackend/java11/gira/user/controller/UserController.java
@@ -1,5 +1,47 @@
 package cybersoft.javabackend.java11.gira.user.controller;
 
-public class UserController {
+import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cybersoft.javabackend.java11.gira.commondata.model.ResponseHandler;
+import cybersoft.javabackend.java11.gira.user.dto.CreateUserDto;
+import cybersoft.javabackend.java11.gira.user.model.User;
+import cybersoft.javabackend.java11.gira.user.service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+	@Autowired
+	private UserService service;
+
+	@GetMapping("")
+	public ResponseEntity<Object> findAllUser(){
+		List<User> users = service.findAll();
+		
+		if(users.isEmpty())
+			return ResponseHandler.getResponse("There is no data.", HttpStatus.OK);
+		
+		return ResponseHandler.getResponse(users, HttpStatus.OK);
+	}
+	
+	@PostMapping("")
+	public ResponseEntity<Object> addNewUser(@Valid @RequestBody CreateUserDto dto,
+											BindingResult errors){
+		if(errors.hasErrors())
+			return ResponseHandler.getResponse(errors, HttpStatus.BAD_REQUEST);
+		
+		User newUser = service.save(dto);
+		return ResponseHandler.getResponse(newUser, HttpStatus.OK);
+	}
 }
--- src/main/java/cybersoft/javabackend/java11/gira/user/service/UserService.java
+++ src/main/java/cybersoft/javabackend/java11/gira/user/service/UserService.java
@@ -1,5 +1,15 @@
 package cybersoft.javabackend.java11.gira.user.service;
 
-public class UserService {
+import cybersoft.javabackend.java11.gira.commondata.GenericService;
+import cybersoft.javabackend.java11.gira.user.dto.CreateUserDto;
+import cybersoft.javabackend.java11.gira.user.model.User;
 
+
+public interface UserService extends GenericService<User, Long> {
+
+	User save(CreateUserDto dto);
+
+	boolean isTakenUsername(String username);
+
+	boolean isTakenEmail(String email);
 }
